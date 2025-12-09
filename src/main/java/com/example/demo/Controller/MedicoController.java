e64b9943ff0ef74fd807990c2f9b00242528cf88
lucianogiacomelli <lucianogiacomelli8@gmail.com>
2025-12-09 20:11:21 -0300

    AUTH0 Funcionando

---
+++ src/main/java/com/example/demo/Controller/MedicoController.java
@@ -0,0 +1,17 @@
+package com.example.demo.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class MedicoController {
+
+
+    @GetMapping("/api/public/ping")
+    public String ping() {
+        return "pong";
+    }
+
+}
